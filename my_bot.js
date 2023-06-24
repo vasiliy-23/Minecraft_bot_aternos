55526683b348c5c20202cda6c6f462421123d8f3
vasiliy-23 <55623358+vasiliy-23@users.noreply.github.com>
2023-06-24 21:00:56 +0200

    Update my_bot.js

--- my_bot.js
+++ my_bot.js
@@ -1,23 +1,23 @@
-//const mineflayer= require('mineflayer')
-//const movement = require("mineflayer-movement")
-//const vec3 = require('vec3')
-//const { pathfinder, Movements, goals} = require('mineflayer-pathfinder')
-//const GoalBlock = goals.GoalBlock
-//const GoalNearXZ = goals.GoalNearXZ
-//const Movements = require('mineflayer-pathfinder').Movements
-//const { GoalNear } = require('mineflayer-pathfinder').goals
-//const bot = mineflayer.createBot({ username: 'Player' })
-//var blockFinderPlugin = require('mineflayer-blockfinder')(mineflayer);
-//const collectBlock = require('mineflayer-collectblock').plugin
-
 const mineflayer = require('mineflayer')
 const pathfinder = require('mineflayer-pathfinder').pathfinder
 const Movements = require('mineflayer-pathfinder').Movements
 const { GoalNear } = require('mineflayer-pathfinder').goals
 
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot Is Ready')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
 const bot = mineflayer.createBot({
-    host: 'localhost', //vasya18jyv8.aternos.me
-    port: '53691', //64810
+    host: 'vasya18jyv8.aternos.me', //localhost
+    port: '64810', //64810
     version: '1.19.4',
     username: 'bot',
     auth: 'offline'
@@ -25,7 +25,12 @@ const bot = mineflayer.createBot({
 
 bot.loadPlugin(pathfinder)
 
-
+//async function move() {
+    //bot.pathfinder.setGoal(new GoalNear(-754, 168, -2126, 1))
+    //await bot.waitForTicks(50)
+    //bot.pathfinder.setGoal(new GoalNear(-754, 168, -2141, 1))
+    //setTimeout(move, 5000)
+//}
 bot.on('spawn', async function move() {
 
     bot.pathfinder.setGoal(new GoalNear(-754, 168, -2126, 1))
@@ -34,7 +39,6 @@ bot.on('spawn', async function move() {
     setTimeout(move, 5000)
 })
 
-
 bot.on('chat', async (username, message) => {
     if (username === bot.username) return
     switch (message) {case 'loaded':
@@ -42,13 +46,12 @@ bot.on('chat', async (username, message) => {
         bot.chat('Я готов!')
         break
 
-        case 'пора работать сука':
-            bot.chat('Погнали нахуй)')
-            move()
-        break
+        //case 'пора работать нахуй':
+            //bot.chat('Погнали нахуй)')
+            //move()
+        //break
     }
 })
-
 
 
 
